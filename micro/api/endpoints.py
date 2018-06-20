323c7dca238005dd4985534405d868c6e732c136
Daniel Espinoza <despinozac@gmail.com>
2018-06-19 23:59:52 -0400

    instance to static methods

--- micro/api/endpoints.py
+++ micro/api/endpoints.py
@@ -1,27 +1,33 @@
 from celery import Celery
 
-app = Celery("Micro", backend="rpc://")
+app = Celery("Micro", backend="amqp://")
 
 
-class Requests:
-    def __init__(self, broker, queue):
+class Request:
+    @staticmethod
+    def credentials(broker, queue):
         app.conf.update(
             broker_url=broker,
             task_routes={"Micro.*": {"queue": queue}}
         )
 
+    @staticmethod
     @app.task(name="Micro.plugins")
     def plugins():
         pass
 
+    @staticmethod
     @app.task(name="Micro.info")
     def info(name):
         pass
 
+    @staticmethod
     @app.task(name="Micro.help")
     def help(name):
         pass
 
+    @staticmethod
     @app.task(name="Micro.run")
     def run(plugin_name, **kwargs):
         pass
+
